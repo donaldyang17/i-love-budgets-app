8cccf3adf69f1b02997fe30e84cbc53d97bcd984
donaldyang <Donald_Yang17@hotmail.com>
2022-11-03 16:04:58 -0400

    added mongoDB connection

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/i-love-budgets", {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
